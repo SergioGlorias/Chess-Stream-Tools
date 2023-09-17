cd7750a7f7e8331c03608851e07dc36cdc2af9b1
Sérgio Glórias <9739913+SergioGlorias@users.noreply.github.com>
2023-09-17 15:48:18 +0100

    profile

---
+++ functions/pt/chatmsg/profile.js
@@ -0,0 +1,23 @@
+export function onRequest(context) {
+    const urlParams = new URLSearchParams(context.request.url);
+    const userParam = urlParams.get("user");
+
+    let user = await fetch("https://lichess.org/api/user/" + userParam, {
+        method: "GET",
+        headers: {
+            Accept: "application/json"
+        }
+    }).then(res => res.json())
+
+    if (user.error) {
+        return new Response("Jogador não encontrado!")
+    }
+
+    if (user.tosViolation) {
+        return new Response(`${user.username} está com uma marca vermelha!`)
+    }
+    
+    return new Response(`${user.username} tem ${user.perfs.bullet.rating}${user.perfs.bullet.prov ? "?" : ""} Bullet, ${user.perfs.blitz.rating}${user.perfs.blitz.prov ? "?" : ""} Rapidas, ${user.perfs.rapid.rating}${user.perfs.rapid.prov ? "?" : ""} Semi-Rápidas e ${user.perfs.classical.rating}${user.perfs.classical.prov ? "?" : ""} Classicas!`)
+
+}
+  
